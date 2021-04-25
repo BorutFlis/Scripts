7f01e0251c749e9b5913aca7dbad6d87f0a506fe
BorutFlis <borutflis1@gmail.com>
2021-04-25 16:41:21 +0200

    Add files via upload

---
+++ twitter_api.py
@@ -0,0 +1,135 @@
+from functools import partial
+from sys import maxsize as maxint
+import sys
+import time
+from urllib.error import URLError
+from http.client import BadStatusLine
+import json
+import twitter
+
+def oauth_login(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN,OAUTH_TOKEN_SECRET):
+    # XXX: Go to http://twitter.com/apps/new to create an app and get values
+    # for these credentials that you'll need to provide in place of these
+    # empty string values that are defined as placeholders.
+    # See https://dev.twitter.com/docs/auth/oauth for more information
+    # on Twitter's OAuth implementation.
+
+
+    auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
+                                      CONSUMER_KEY, CONSUMER_SECRET)
+
+    twitter_api = twitter.Twitter(auth=auth)
+    return twitter_api
+
+
+def make_twitter_request(twitter_api_func, max_errors=10, *args, **kw):
+    # A nested helper function that handles common HTTPErrors. Return an updated
+    # value for wait_period if the problem is a 500 level error. Block until the
+    # rate limit is reset if it's a rate limiting issue (429 error). Returns None
+    # for 401 and 404 errors, which requires special handling by the caller.
+    def handle_twitter_http_error(e, wait_period=2, sleep_when_rate_limited=True):
+
+        if wait_period > 3600:  # Seconds
+            print('Too many retries. Quitting.', file=sys.stderr)
+            raise e
+
+        # See https://developer.twitter.com/en/docs/basics/response-codes
+        # for common codes
+
+        if e.e.code == 401:
+            print('Encountered 401 Error (Not Authorized)', file=sys.stderr)
+            return None
+        elif e.e.code == 404:
+            print('Encountered 404 Error (Not Found)', file=sys.stderr)
+            return None
+        elif e.e.code == 429:
+            print('Encountered 429 Error (Rate Limit Exceeded)', file=sys.stderr)
+            if sleep_when_rate_limited:
+                print("Retrying in 15 minutes...ZzZ...", file=sys.stderr)
+                sys.stderr.flush()
+                time.sleep(60 * 15 + 5)
+                print('...ZzZ...Awake now and trying again.', file=sys.stderr)
+                return 2
+            else:
+                raise e  # Caller must handle the rate limiting issue
+        elif e.e.code in (500, 502, 503, 504):
+            print('Encountered {0} Error. Retrying in {1} seconds' \
+                  .format(e.e.code, wait_period), file=sys.stderr)
+            time.sleep(wait_period)
+            wait_period *= 1.5
+            return wait_period
+        else:
+            raise e
+
+    # End of nested helper function
+
+    wait_period = 2
+    error_count = 0
+
+    while True:
+        try:
+            return twitter_api_func(*args, **kw)
+        except twitter.api.TwitterHTTPError as e:
+            error_count = 0
+            wait_period = handle_twitter_http_error(e, wait_period)
+            if wait_period is None:
+                return
+        except URLError as e:
+            error_count += 1
+            time.sleep(wait_period)
+            wait_period *= 1.5
+            print("URLError encountered. Continuing.", file=sys.stderr)
+            if error_count > max_errors:
+                print("Too many consecutive errors...bailing out.", file=sys.stderr)
+                raise
+        except BadStatusLine as e:
+            error_count += 1
+            time.sleep(wait_period)
+            wait_period *= 1.5
+            print("BadStatusLine encountered. Continuing.", file=sys.stderr)
+            if error_count > max_errors:
+                print("Too many consecutive errors...bailing out.", file=sys.stderr)
+                raise
+
+
+def twitter_search(twitter_api, q, max_results=200, **kw):
+    # See https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets
+    # and https://developer.twitter.com/en/docs/tweets/search/guides/standard-operators
+    # for details on advanced search criteria that may be useful for
+    # keyword arguments
+
+    # See https://dev.twitter.com/docs/api/1.1/get/search/tweets
+    search_results = twitter_api.search.tweets(q=q, count=100, **kw)
+
+    statuses = search_results['statuses']
+
+    # Iterate through batches of results by following the cursor until we
+    # reach the desired number of results, keeping in mind that OAuth users
+    # can "only" make 180 search queries per 15-minute interval. See
+    # https://developer.twitter.com/en/docs/basics/rate-limits
+    # for details. A reasonable number of results is ~1000, although
+    # that number of results may not exist for all queries.
+
+    # Enforce a reasonable limit
+    max_results = min(1000, max_results)
+
+    for _ in range(10):  # 10*100 = 1000
+        try:
+            next_results = search_results['search_metadata']['next_results']
+        except KeyError as e:  # No more results when next_results doesn't exist
+            break
+
+        # Create a dictionary from next_results, which has the following form:
+        # ?max_id=313519052523986943&q=NCAA&include_entities=1
+        kwargs = dict([kv.split('=')
+                       for kv in next_results[1:].split("&")])
+
+        search_results = twitter_api.search.tweets(**kwargs)
+        statuses += search_results['statuses']
+
+        if len(statuses) > max_results:
+            break
+
+    return statuses
+
+
